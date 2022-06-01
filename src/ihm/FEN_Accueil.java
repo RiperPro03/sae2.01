c73cbc6aa2721338b5c26069aac0aaa4ffb5cd59
Theo Lemoine <theo.lemoine@etu.iut-tlse3.fr>
2022-06-01 15:36:52 +0200

    fenêtre Théo

---
+++ src/ihm/FEN_Accueil.java
@@ -0,0 +1,111 @@
+package ihm;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Component;
+import javax.swing.JScrollBar;
+import java.awt.Color;
+import javax.swing.JSlider;
+
+public class FEN_Accueil {
+
+	private JFrame frame;
+	private JTextField txtFromageQuiRit;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FEN_Accueil window = new FEN_Accueil();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FEN_Accueil() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+		
+		JPanel header = new JPanel();
+		header.setBorder(null);
+		frame.getContentPane().add(header);
+		header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
+		
+		JLabel image_header = new JLabel("Attente image");
+		header.add(image_header);
+		
+		txtFromageQuiRit = new JTextField();
+		txtFromageQuiRit.setForeground(Color.BLACK);
+		txtFromageQuiRit.setAlignmentX(Component.LEFT_ALIGNMENT);
+		txtFromageQuiRit.setBackground(Color.WHITE);
+		txtFromageQuiRit.setFont(new Font("Roboto Light", Font.PLAIN, 14));
+		txtFromageQuiRit.setHorizontalAlignment(SwingConstants.CENTER);
+		txtFromageQuiRit.setEditable(false);
+		txtFromageQuiRit.setText("Fromage qui rit");
+		header.add(txtFromageQuiRit);
+		txtFromageQuiRit.setColumns(1);
+		
+		JButton btn_panier = new JButton("indic_panier");
+		header.add(btn_panier);
+		
+		JPanel sélection_type = new JPanel();
+		sélection_type.setBorder(null);
+		frame.getContentPane().add(sélection_type);
+		sélection_type.setLayout(new GridLayout(0, 4, 0, 0));
+		
+		JButton btn_tout_fromage = new JButton("Tout");
+		sélection_type.add(btn_tout_fromage);
+		
+		JButton btn_fromage_brebis = new JButton("Attente icon b");
+		sélection_type.add(btn_fromage_brebis);
+		
+		JButton btn_fromage_vache = new JButton("Attente icon v");
+		sélection_type.add(btn_fromage_vache);
+		
+		JButton btn_fromage_chèvre = new JButton("Attente icon");
+		sélection_type.add(btn_fromage_chèvre);
+		
+		JPanel parcours_fromage = new JPanel();
+		frame.getContentPane().add(parcours_fromage);
+		parcours_fromage.setLayout(new BorderLayout(0, 0));
+		
+		JList liste_fromage = new JList();
+		parcours_fromage.add(liste_fromage, BorderLayout.CENTER);
+		
+		JSlider slider = new JSlider();
+		slider.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		slider.setOrientation(SwingConstants.VERTICAL);
+		parcours_fromage.add(slider, BorderLayout.EAST);
+	}
+
+}
---
+++ src/ihm/FEN_Détails_Fromage.java
@@ -0,0 +1,109 @@
+package ihm;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.JTextArea;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
+import javax.swing.JTextPane;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JSpinner;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.DefaultComboBoxModel;
+
+public class FEN_Détails_Fromage {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FEN_Détails_Fromage window = new FEN_Détails_Fromage();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FEN_Détails_Fromage() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+		
+		JPanel NORTH = new JPanel();
+		frame.getContentPane().add(NORTH);
+		
+		JLabel Titre_Détails_Fromage = new JLabel("Nom frometon");
+		NORTH.add(Titre_Détails_Fromage);
+		
+		JPanel détails_du_fromage = new JPanel();
+		frame.getContentPane().add(détails_du_fromage);
+		détails_du_fromage.setLayout(new BorderLayout(0, 0));
+		
+		JSlider slider = new JSlider();
+		slider.setOrientation(SwingConstants.VERTICAL);
+		détails_du_fromage.add(slider, BorderLayout.EAST);
+		
+		JTextArea information_fromage = new JTextArea();
+		information_fromage.setText("Bon fromage croie le");
+		information_fromage.setEditable(false);
+		détails_du_fromage.add(information_fromage, BorderLayout.CENTER);
+		
+		JPanel FOOTER = new JPanel();
+		frame.getContentPane().add(FOOTER);
+		FOOTER.setLayout(new GridLayout(2, 0, 0, 0));
+		
+		JPanel South_North = new JPanel();
+		FlowLayout fl_South_North = (FlowLayout) South_North.getLayout();
+		fl_South_North.setAlignment(FlowLayout.LEFT);
+		FOOTER.add(South_North);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Type de vente : Prix TTC"}));
+		South_North.add(comboBox);
+		
+		JSpinner spinner_nb_fromage = new JSpinner();
+		South_North.add(spinner_nb_fromage);
+		
+		JPanel South_South = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) South_South.getLayout();
+		flowLayout.setAlignment(FlowLayout.RIGHT);
+		FOOTER.add(South_South);
+		
+		JButton btn_ajouter_panier = new JButton("Ajouter au panier");
+		btn_ajouter_panier.setForeground(new Color(0, 0, 0));
+		btn_ajouter_panier.setBackground(new Color(50, 205, 50));
+		South_South.add(btn_ajouter_panier);
+		
+		JButton btn_annuler_ajout = new JButton("Annuler");
+		btn_annuler_ajout.setBackground(new Color(255, 69, 0));
+		South_South.add(btn_annuler_ajout);
+	}
+
+}
