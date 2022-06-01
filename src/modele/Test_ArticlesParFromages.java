88632a99e02a5053bc7271add0860f9484ecb67a
Blibox <95865510+Blibox@users.noreply.github.com>
2022-06-01 14:54:15 +0200

    Création fichiers test

---
+++ src/modele/Test_ArticlesParFromages.java
@@ -0,0 +1,14 @@
+package modele;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_ArticlesParFromages {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/modele/Test_Panier.java
@@ -0,0 +1,14 @@
+package modele;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_Panier {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
