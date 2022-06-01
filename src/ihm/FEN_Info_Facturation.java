0a0cc2de7cf8c8d6ae860b07d611606943ea2f3a
Etudiant <Etudiant@R-INFO-PRET-T14>
2022-06-01 15:35:55 +0200

    fenêtre baptiste

---
+++ src/ihm/FEN_Info_Facturation.java
@@ -0,0 +1,117 @@
+package ihm;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import java.awt.Color;
+import javax.swing.JTextField;
+
+public class FEN_Info_Facturation {
+
+	private JFrame frame;
+	private JTable table1;
+	private JTable table2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FEN_Info_Facturation window = new FEN_Info_Facturation();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FEN_Info_Facturation() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel North = new JPanel();
+		frame.getContentPane().add(North, BorderLayout.NORTH);
+		
+		JLabel titreInfoFacture = new JLabel("Information facture");
+		titreInfoFacture.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		North.add(titreInfoFacture);
+		
+		JPanel South = new JPanel();
+		frame.getContentPane().add(South, BorderLayout.SOUTH);
+		South.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
+		
+		JButton Bouton1 = new JButton("Valider");
+		Bouton1.setBackground(Color.GREEN);
+		South.add(Bouton1);
+		
+		JButton Bouton2 = new JButton("Annuler");
+		Bouton2.setBackground(Color.RED);
+		Bouton2.setForeground(Color.BLACK);
+		South.add(Bouton2);
+		
+		JPanel Center = new JPanel();
+		frame.getContentPane().add(Center, BorderLayout.CENTER);
+		Center.setLayout(new BoxLayout(Center, BoxLayout.X_AXIS));
+		
+		table1 = new JTable();
+		table1.setEnabled(false);
+		table1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		table1.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"Nom"},
+				{"Pr\u00E9nom"},
+				{"Adresse 1"},
+				{"Code Postal"},
+				{"Ville"},
+				{"T\u00E9l\u00E9phone"},
+				{"Mail"},
+			},
+			new String[] {
+				"Info"
+			}
+		));
+		Center.add(table1);
+		
+		table2 = new JTable();
+		table2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		table2.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+			},
+			new String[] {
+				"New column"
+			}
+		));
+		Center.add(table2);
+	}
+
+}
---
+++ src/ihm/FEN_Panier.java
@@ -0,0 +1,143 @@
+package ihm;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.JLabel;
+import javax.swing.DropMode;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import java.awt.GridLayout;
+import java.awt.CardLayout;
+import javax.swing.JSlider;
+import javax.swing.JComboBox;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class FEN_Panier {
+
+	private JFrame frame;
+	private JPanel South;
+	private JPanel Center;
+	private JTable table;
+	private JSlider slider;
+	private JPanel South2;
+	private JComboBox comboBox;
+	private JTextField txtModeDeLivraison;
+	private JPanel South3;
+	private JButton Bouton1;
+	private JButton Bouton2;
+	private JButton Bouton3;
+	private JLabel titrePanier;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FEN_Panier window = new FEN_Panier();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FEN_Panier() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel North = new JPanel();
+		frame.getContentPane().add(North, BorderLayout.NORTH);
+		
+		titrePanier = new JLabel("Panier");
+		titrePanier.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		North.add(titrePanier);
+		
+		South = new JPanel();
+		frame.getContentPane().add(South, BorderLayout.SOUTH);
+		South.setLayout(new GridLayout(2, 0, 0, 0));
+		
+		South2 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) South2.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		South.add(South2);
+		
+		txtModeDeLivraison = new JTextField();
+		txtModeDeLivraison.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txtModeDeLivraison.setText("Mode de livraison");
+		txtModeDeLivraison.setEditable(false);
+		South2.add(txtModeDeLivraison);
+		txtModeDeLivraison.setColumns(10);
+		
+		comboBox = new JComboBox();
+		South2.add(comboBox);
+		
+		South3 = new JPanel();
+		FlowLayout flowLayout_1 = (FlowLayout) South3.getLayout();
+		flowLayout_1.setAlignment(FlowLayout.RIGHT);
+		South.add(South3);
+		
+		Bouton1 = new JButton("Commander");
+		Bouton1.setForeground(Color.BLACK);
+		Bouton1.setBackground(Color.GREEN);
+		South3.add(Bouton1);
+		
+		Bouton2 = new JButton("Vider le panier");
+		Bouton2.setBackground(Color.RED);
+		South3.add(Bouton2);
+		
+		Bouton3 = new JButton("Continuer les achats");
+		Bouton3.setBackground(Color.BLUE);
+		South3.add(Bouton3);
+		
+		Center = new JPanel();
+		frame.getContentPane().add(Center, BorderLayout.CENTER);
+		Center.setLayout(new BoxLayout(Center, BoxLayout.X_AXIS));
+		
+		table = new JTable();
+		table.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"Produits", "Prix", "Quantit\u00E9", "Total"},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"Produits", "Prix", "Quantit\u00E9", "Total"
+			}
+		));
+		Center.add(table);
+		
+		slider = new JSlider();
+		slider.setOrientation(SwingConstants.VERTICAL);
+		Center.add(slider);
+	}
+
+}
