29af4bcefe0ca57598ef7f3c7803dc818e302cd4
Blibox <95865510+Blibox@users.noreply.github.com>
2022-06-04 00:21:53 +0200

    Amélioration mode de livraison pour simplifier l'IHM

--- src/modele/ModeLivraison.java
+++ src/modele/ModeLivraison.java
@@ -1,6 +1,10 @@
 package modele;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public enum ModeLivraison {
-	DHL(4.5f,"DHL \t 4,50€"),COLISSIMO(9.9F,"Colissimo \t 9,90€"),MONDIAL_RELAI(6f,"Mondial Relai \t 6€");
+	DHL(4.5f,"DHL"),COLISSIMO(9.9F,"Colissimo"),MONDIAL_RELAI(6f,"Mondial Relai");
 	
 	private float prix;
 	private String nom;
@@ -10,10 +14,25 @@ public enum ModeLivraison {
 		this.nom = nom;
 	}
 	public String toString() {
-		return nom;
+		return String.format("%10s",nom ) + String.format("%10.2f", prix);
 	}
 	public float getPrix() {
 		return prix;
+	}
+	public static List<String> getAllValues() {
+		List<String> result = new LinkedList<String>();
+		for (ModeLivraison m : ModeLivraison.values()) {
+			result.add(m.toString());						
+		}
+		return result;
+	}
+	public ModeLivraison getPrix(String value) {
+		for(ModeLivraison m : ModeLivraison.values()) {
+			if(m.toString() == value) {
+				return m;
+			}
+		}
+		return null;
 	}
 }
 
